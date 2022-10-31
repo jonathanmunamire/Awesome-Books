bb9450394501df865e8a6fdaaf6c2a55ab3c738e
jonathanmunamire <jonathanmunamire@gmail.com>
2022-10-31 22:42:28 +0200

    Set up Modules in JS

--- index.js
+++ index.js
@@ -1,60 +1,13 @@
+import { Book } from "./modules/BookStorage.js";
+import { showBookPage,showFormPage,showContactPage } from "./modules/Navigation.js";
+import { currentTime } from "./modules/Date.js";
+
 const bookList = document.getElementById('book-list');
 const newBookForm = document.getElementById('new-book-form');
 const inputTitle = document.getElementById('input-title');
 const inputAuthor = document.getElementById('input-author');
 const successSmall = document.getElementById('success-small');
 
-// we have  a class Book
-// a static array to contain all the instances
-// method add elements to the array,
-// delete element  from the array
-// method to return array
-
-class Book {
-  static booksArray = []
-
-  static count = 0
-
-  static storageUpdater;
-
-  static setUpdater(value) {
-    Book.storageUpdater = value;
-  }
-
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-
-    Book.booksArray.push(this);
-    if (typeof Book.storageUpdater === 'function') {
-      Book.storageUpdater();
-    }
-  }
-
-  deleteBook() {
-    const i = Book.booksArray.indexOf(this);
-    Book.booksArray.splice(i, 1);
-    if (typeof Book.storageUpdater === 'function') {
-      Book.storageUpdater();
-    }
-  }
-
-  static getAllBooksArray() {
-    return Book.booksArray;
-  }
-
-  static updateBooksArray(dataSource) {
-    if (dataSource?.length) {
-      const newArray = dataSource.map((rawBook) => {
-        const book = new Book(rawBook.title, rawBook.author);
-        return book;
-      });
-
-      Book.booksArray = [];
-      Book.booksArray.push(...newArray);
-    }
-  }
-}
 
 function storeBooksToStorage() {
   const booksString = JSON.stringify(Book.getAllBooksArray());
@@ -64,6 +17,29 @@ function storeBooksToStorage() {
 Book.setUpdater(storeBooksToStorage);
 
 Book.updateBooksArray(JSON.parse(window.localStorage.getItem('BookData')));
+
+
+function populateOriginalBookList(books) {
+  books.forEach((book) => {
+    const bItem = createBook(book);
+    bookList.append(bItem);
+  });
+}
+
+function displayOneBook(book) {
+  const bItem = createBook(book);
+  bookList.append(bItem);
+  
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const booksArray = Book.getAllBooksArray();
+  if (booksArray.length) {
+    populateOriginalBookList(booksArray);
+  }
+
+  
+});
 
 function createBook(book) {
   const mainTr = document.createElement('tr');
@@ -84,24 +60,7 @@ function createBook(book) {
   return mainTr;
 }
 
-function populateOriginalBookList(books) {
-  books.forEach((book) => {
-    const bItem = createBook(book);
-    bookList.append(bItem);
-  });
-}
-
-function displayOneBook(book) {
-  const bItem = createBook(book);
-  bookList.append(bItem);
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  const booksArray = Book.getAllBooksArray();
-  if (booksArray.length) {
-    populateOriginalBookList(booksArray);
-  }
-});
+export {createBook};
 
 newBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -119,30 +78,6 @@ newBookForm.addEventListener('submit', (event) => {
   newBookForm.reset();
 });
 
-/// /////////////////////////////////////Navigation//////////////////////////
-
-const bookPage = document.getElementById('book-page');
-const formPage = document.getElementById('form-page');
-const contactPage = document.getElementById('contact-page');
-
-function showBookPage() {
-  formPage.classList.add('hide');
-  contactPage.classList.add('hide');
-  bookPage.classList.remove('hide');
-}
-
-function showFormPage() {
-  formPage.classList.remove('hide');
-  contactPage.classList.add('hide');
-  bookPage.classList.add('hide');
-}
-
-function showContactPage() {
-  formPage.classList.add('hide');
-  contactPage.classList.remove('hide');
-  bookPage.classList.add('hide');
-}
-
 window.addEventListener('hashchange', () => {
   const { hash } = window.location;
   if (hash === '#book-page') {
@@ -153,3 +88,9 @@ window.addEventListener('hashchange', () => {
     showContactPage();
   }
 }, false);
+
+setInterval(() => {
+  currentTime();
+  }, 1000); 
+
+
---
+++ modules/Navigation.js
@@ -0,0 +1,23 @@
+const bookPage = document.getElementById('book-page');
+const formPage = document.getElementById('form-page');
+const contactPage = document.getElementById('contact-page');
+
+function showBookPage() {
+  formPage.classList.add('hide');
+  contactPage.classList.add('hide');
+  bookPage.classList.remove('hide');
+}
+
+function showFormPage() {
+  formPage.classList.remove('hide');
+  contactPage.classList.add('hide');
+  bookPage.classList.add('hide');
+}
+
+function showContactPage() {
+  formPage.classList.add('hide');
+  contactPage.classList.remove('hide');
+  bookPage.classList.add('hide');
+}
+
+export { showBookPage,showFormPage,showContactPage };
